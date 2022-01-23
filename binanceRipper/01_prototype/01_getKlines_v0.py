5cf5803a354177146f1f97f1e68945422d3f0945
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-01-23 13:58:42 +0800

    Example code
    Basic code for getting klines and saving it as CSV file.

---
+++ binanceRipper/01_prototype/01_getKlines_v0.py
@@ -0,0 +1,20 @@
+from binance.client import Client
+import csv
+
+client = Client()
+
+SYMBOL = 'BTCUSDT'
+
+columns = [
+    'open_time', 'open', 'high', 'low', 'close', 'volume',
+    'close_time', 'quote_asset_volume', 'number_of_trades',
+    'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
+    'ignore'
+]
+
+klines = client.get_historical_klines(SYMBOL, Client.KLINE_INTERVAL_1WEEK, "1 Jan, 2021")
+
+with open('1week.csv', 'w', newline='') as f:
+    write = csv.writer(f)
+    write.writerow(columns)
+    write.writerows(klines)
