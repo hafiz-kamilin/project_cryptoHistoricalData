73326012fd45a66dba87a518b1be304acfe96182
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-01-27 22:03:57 +0800

    Create 03_asyncGetKlines_v04.py

---
+++ binanceRipper/01_prototype/03_asyncGetKlines_v04.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Mohd Hafizuddin Bin Kamilin"
+__version__ = "0.0.4"
+__date__ = "27 May 2022"
+
+"""
+    Sample program to get custom trading pair and timerange from binance via async
+
+    + TODO add custom date range
+    + TODO add write to csv
+
+"""
