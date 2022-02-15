368e5348d2a22d3060583e721bdfc3c2be6a188f
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-02-15 22:13:04 +0800

    Create test2.py

---
+++ source/01_prototype/cex_binance/v2.0/test2.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+timestamp = 1609430400
+dt_object = datetime.fromtimestamp(timestamp)
+
+print("dt_object =", dt_object)
+print("type(dt_object) =", type(dt_object))
