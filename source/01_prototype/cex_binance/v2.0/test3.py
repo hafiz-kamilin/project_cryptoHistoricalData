3423226389ae93fe413eea74ef11f27373764e9b
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-02-15 22:33:09 +0800

    Create test3.py

---
+++ source/01_prototype/cex_binance/v2.0/test3.py
@@ -0,0 +1,24 @@
+from dateutil.parser import parse
+from datetime import datetime
+import time
+
+from numpy import divide
+
+parsedStartDateTime = parse('1 Jan, 2021')
+parsedEndDateTime = parse('1 Jan, 2022')
+
+startTimeStamp = int(time.mktime(parsedStartDateTime.timetuple()))
+endTimeStamp = int(time.mktime(parsedEndDateTime.timetuple()))
+
+timeStampDifference = endTimeStamp - startTimeStamp
+
+dividedTimeStamp = timeStampDifference / 12
+
+timeStampList = []
+for i in range(12 + 1):
+
+    timeStampList.append(
+        datetime.fromtimestamp(int(startTimeStamp + dividedTimeStamp * i))
+    )
+
+print(timeStampList) 
