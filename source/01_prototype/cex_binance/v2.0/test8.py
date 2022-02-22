0f2fa2144da94ffd52e4d9ce5b55d0a8ddef1663
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-02-22 23:30:20 +0800

    Update test8.py

--- source/01_prototype/cex_binance/v2.0/test8.py
+++ source/01_prototype/cex_binance/v2.0/test8.py
@@ -1,37 +1,11 @@
+from binance import AsyncClient
 import asyncio
- 
-async def worker():
-    print("Starting work")
-    await asyncio.sleep(5)
-    print("Work complete")
- 
-async def other_worker():
-    print("Starting other worker")
-    await asyncio.sleep(2)
-    print("Other worker complete")
- 
-async def third_worker():
-    print("Starting third worker")
-    await asyncio.sleep(1)
-    print("Third worker complete")
- 
-async def main():
-    print("Starting workers")
-    tasks = []
-    tasks.append(asyncio.ensure_future(worker()))
-    tasks.append(asyncio.ensure_future(other_worker()))
- 
-    print("First two workers added to the event loop.")
-    for task in tasks:
-        await task
- 
-    print("Adding third worker to the event loop.")
-    await third_worker()
-    # in this case, this is the same thing
-    await asyncio.ensure_future(third_worker())
-    
-    await third_worker()
-    # in this case, this is the same thing
-    await asyncio.ensure_future(third_worker())
- 
-asyncio.run(main())
+
+class Demo:
+
+    def measure_weight_by_call(self) -> int:
+
+        interval = "1m"
+        
+
+        pass
