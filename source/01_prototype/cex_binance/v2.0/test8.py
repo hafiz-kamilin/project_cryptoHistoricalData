0163856a67f2cc05574a602b016b253e76a201a3
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-03-03 11:04:42 +0800

    Update test8.py

--- source/01_prototype/cex_binance/v2.0/test8.py
+++ source/01_prototype/cex_binance/v2.0/test8.py
@@ -1,11 +1,68 @@
-from binance import AsyncClient
+from binance import Client, AsyncClient
+from time import sleep
 import asyncio
+import csv
 
-class Demo:
+class GetAllBinanceData:
 
-    def measure_weight_by_call(self) -> int:
+    def __init__(self):
 
-        interval = "1m"
-        
+        self.weight = 0
+        self.count = 0
+        self.symbols = [
+            "BTCUSDT", 
+            "PUNDIXUSDT", 
+            "BCHUPUSDT", 
+            "1INCHUSDT", 
+            "CFXUSDT", 
+            "WNXMUSDT", 
+            "SANDUSDT", 
+            "STRAXUSDT", 
+            "FLMUSDT", 
+            "WINGUSDT"
+        ]
 
-        pass
+    async def test_call(self, weight, client: AsyncClient):
+
+        if weight < 10:
+
+            symbol = self.symbols.pop()
+
+            # get kline based on the predefined kline interval
+            klines = await client.get_historical_klines(
+                symbol=symbol,
+                interval="1m",
+                start_str="2021-1-1 00:00:00",
+                end_str="2021-1-31 00:00:00"
+            )
+
+            with open(symbol + '.csv', 'w', newline='') as f:
+                write = csv.writer(f)
+                write.writerows(klines)
+
+            self.weight = int(Client().response.headers["x-mbx-used-weight"])
+            print("Weight " + str(self.weight))
+            self.count += 1
+            print("Count " + str(self.count))
+
+        else:
+
+            pass
+
+    async def amain(self) -> None:
+
+        client = await AsyncClient.create()
+
+        await asyncio.gather(
+            *(
+                self.test_call(self.weight, client) for _ in range(10)
+            )
+        )
+
+        await client.close_connection()
+
+if __name__ == "__main__":
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(GetAllBinanceData().amain())
+    
