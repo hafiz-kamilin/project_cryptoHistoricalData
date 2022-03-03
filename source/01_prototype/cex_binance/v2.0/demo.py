57ff112b79950bf80e0a949feb15a8a4e15025ee
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-03-03 19:55:22 +0800

    Update demo.py

--- source/01_prototype/cex_binance/v2.0/demo.py
+++ source/01_prototype/cex_binance/v2.0/demo.py
@@ -1,8 +1,19 @@
-# use python-binance API to interact with binance
-from binance.client import Client
+import logging
 
-client = Client()
-client.get_exchange_info()
-print(client.response.headers["x-mbx-used-weight"])
-print(client.response.headers["x-mbx-used-weight"])
-print(client.response.headers["x-mbx-used-weight"])
+import logging
+
+# logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+# logging.debug('This message should go to\n the log file')
+# logging.info('So should this')
+# logging.warning('And this, too')
+# logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+
+# logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.warning('This will get logged to a file')
+
+logging.info('This is an info message')
+a = logging.getLoggerClass().root.handlers
+print(logging.getLoggerClass().root.handlers)
