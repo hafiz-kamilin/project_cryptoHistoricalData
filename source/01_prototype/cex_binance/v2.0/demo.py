9f883b21d2ab8c6ffdbe33f85e875ae172479104
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-03-04 22:15:30 +0800

    Update demo.py

--- source/01_prototype/cex_binance/v2.0/demo.py
+++ source/01_prototype/cex_binance/v2.0/demo.py
@@ -1,7 +1,9 @@
 start = 0
-end = 50
+end = 100
 
 divisor = 7
+
+concurrent_limit = 10
 
 quotient = int(end / divisor)
 remainder = end % divisor
@@ -39,3 +41,20 @@ else:
     start += remainder
     divided_end.append(start)
     print(str(divided_start[0]) + " - " + str(divided_end[0]))
+
+
+
+
+# source: https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
+def divide_chunks(l, n):
+      
+    # looping till length l
+    for i in range(0, len(l), n): 
+        yield l[i:i + n]
+  
+# How many elements each
+# list should have
+n = 5
+  
+x = list(divide_chunks(divided_start, n))
+print (x)
