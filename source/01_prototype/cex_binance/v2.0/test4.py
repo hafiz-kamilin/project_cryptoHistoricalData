8a4d13a6b4dc87d978d98ee70e7d807ed7e071ff
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-03-05 22:30:01 +0800

    Update test4.py

--- source/01_prototype/cex_binance/v2.0/test4.py
+++ source/01_prototype/cex_binance/v2.0/test4.py
@@ -7,8 +7,8 @@ from numpy import divide
 # 1 day 86,400,000
 # 1 minute 60,000
 
-parsedStartDateTime = parse('1 Jan, 2021')
-parsedEndDateTime = parse('1 Jan, 2022')
+parsedStartDateTime = parse('2022-1-1 00:00:00')
+parsedEndDateTime = parse('2022-1-2 00:00:00')
 
 startTimeStamp = int(time.mktime(parsedStartDateTime.timetuple()))
 endTimeStamp = int(time.mktime(parsedEndDateTime.timetuple()))
