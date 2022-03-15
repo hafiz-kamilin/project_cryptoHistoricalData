4a1389c99f90f8f5a51b7222f513094df7b7fbec
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-03-15 23:23:10 +0800

    Update time_splitter.py

--- source/01_exchange/cex_binance/new_version/companion_code/time_splitter.py
+++ source/01_exchange/cex_binance/new_version/companion_code/time_splitter.py
@@ -11,10 +11,10 @@ from datetime import datetime
 # timezone manipulation
 import pytz
 
-def time_splitter(concurrent_limit: int, start: str, end: str) -> tuple[list[list[str]], list[list[str]]]:
+def time_splitter(start: str, end: str, duration_limit: int, concurrent_limit: int) -> tuple[list[list[str]], list[list[str]]]:
 
     """
-    slice the time according to the divisor and group as chuck 
+    slice the time according to the duration_limit and group as chuck 
 
     NOTE: we need to splice time duration specified by the user into month and
         group it as a chunk of 10 months, to prevent exceeding 1200 request
@@ -23,14 +23,11 @@ def time_splitter(concurrent_limit: int, start: str, end: str) -> tuple[list[lis
     
     """
 
-    def divide_chunks(l: list) -> list[list[str]]:
+    def list_segmenter(concurrent_limit: int, l: list) -> list[list]:
 
         """
         divide the splitted time (splitted_start and splitted_end) into a
-        nested list
-
-        NOTE: the concurrent function to fetch the klines will read the nested
-            list and fetch the klines based on the defined start/end time
+        nested list, where each of the nested list has int(concurrent_limit) length
 
         """
         
@@ -42,64 +39,84 @@ def time_splitter(concurrent_limit: int, start: str, end: str) -> tuple[list[lis
 
         return processed
 
+    # to record the splitted time
     splitted_start = []
     splitted_end = []
 
-    # 1 day     86,400 s  →  86,400 timestamp  →  86,400,000 binance's timestamp
-    # 1 hour    3,600 s   →  3,600 timestamp   →  3,600,000 binance's timestamp
-    # 1 minute  60 s      →  60 timestamp      →  60,000 binance's timestamp
-
     timestamp_in_1m = 60
-    # NOTE: (timestamp for 1 minute) * 60 minutes * 24 hours * 7 days * 4 weeks
-    divisor = timestamp_in_1m * 60 * 24 * 7 * 4
 
     # we specifically use UTC timezone to match with the binance API timezone
     tz = pytz.timezone("UTC")
     # convert the date time str to <class 'datetime.datetime'>
-    calculated_time = parse(start).replace(tzinfo=pytz.UTC)
+    start_time = parse(start).replace(tzinfo=pytz.UTC)
     time_duration = parse(end).replace(tzinfo=pytz.UTC)
     # convert the datetime to timestamp (utc)
-    calculated_time = int(datetime.timestamp(calculated_time))
-    time_duration = int(datetime.timestamp(time_duration) - calculated_time)
+    start_time = int(datetime.timestamp(start_time))
+    time_duration = int(datetime.timestamp(time_duration) - start_time)
 
-    # if the time_duration is larger than the divisor
-    if time_duration > divisor:
+    # if the time_duration is more than duration_limit
+    if time_duration > duration_limit:
 
-        # find out how many times we can divide the time duration with the divisor and its remainder
-        quotient = int(time_duration / divisor)
-        remainder = time_duration % divisor
+        # find out how many times we can divide the time_duration with the duration_limit and its remainder
+        quotient = int(time_duration / duration_limit)
+        remainder = time_duration % duration_limit
 
-        for i in range(quotient):
+        for i in range(quotient * concurrent_limit):
 
             # append the newly calculated start time to the splitted_start
             if i == 0:
-                splitted_start.append(str(datetime.fromtimestamp(calculated_time, tz))[:-6])
+                splitted_start.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
             else:
-                splitted_start.append(str(datetime.fromtimestamp(calculated_time + timestamp_in_1m, tz))[:-6])
+                # NOTE: smallest klines interval is 1 minute
+                splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
 
             # append the newly calculated end time to the splitted_end
-            calculated_time += divisor
-            splitted_end.append(str(datetime.fromtimestamp(calculated_time, tz))[:-6])
+            start_time += duration_limit / concurrent_limit
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
 
         # if there is a remainder from the division
         if remainder != 0:
 
             # append the newly calculated start time to the splitted_start
-            splitted_start.append(str(datetime.fromtimestamp(calculated_time + timestamp_in_1m, tz))[:-6])
+            splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
 
             # append the newly calculated end time to the splitted_end
-            calculated_time += remainder
-            splitted_end.append(str(datetime.fromtimestamp(calculated_time, tz))[:-6])
+            start_time += remainder
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
 
-    # if the time_duration is equal or less than the divisor
+    # if the time_duration is equal or less than the duration_limit
     else:
+
+        # find out how many times we can divide the time_duration with the concurrent_limit and its remainder
+        remainder = time_duration % concurrent_limit
+        quotient = int(time_duration / concurrent_limit)
+        number_of_loop = (concurrent_limit if (remainder == 0) else (concurrent_limit - 1))
         
-        # no calculation needed
-        splitted_start.append(start)
-        splitted_end.append(end)
+        for i in range(number_of_loop):
+
+            # append the newly calculated start time to the splitted_start
+            if i == 0:
+                splitted_start.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+            else:
+                # NOTE: smallest klines interval is 1 minute
+                splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
+
+            # append the newly calculated end time to the splitted_end
+            start_time += quotient
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+
+        # if there is a remainder from the division
+        if remainder != 0:
+
+            # append the newly calculated start time to the splitted_start
+            splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
+
+            # append the newly calculated end time to the splitted_end
+            start_time += remainder
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
 
     # create a nested list of `self.concurrent_limit` months
-    splitted_start = divide_chunks(splitted_start)
-    splitted_end = divide_chunks(splitted_end)
+    splitted_start = list_segmenter(concurrent_limit, splitted_start)
+    splitted_end = list_segmenter(concurrent_limit, splitted_end)
 
     return splitted_start, splitted_end
