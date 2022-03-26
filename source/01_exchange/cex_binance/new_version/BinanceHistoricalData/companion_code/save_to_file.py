daef1d6e352b2ee317e5f85cb7c56ce3eb2d1053
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2022-03-26 20:59:05 +0800

    Moved the program into a different folder.

---
+++ source/01_exchange/cex_binance/new_version/BinanceHistoricalData/companion_code/save_to_file.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Mohd Hafizuddin Bin Kamilin"
+__date__ = "7 March 2022"
+
+# converting datetime in str into an object
+from dateutil.parser import parse
+# to get the current datetime
+from datetime import datetime
+# data processing and saving as feather
+import pandas as pd
+# data saving as pickle
+import pickle
+# timezone manipulation
+import pytz
+# data saving as csv
+import csv
+
+def save_to_file(file_format: str, pair: str, start: str, end: str, interval: str, rearranged_klines: list) -> None:
+
+    """
+    save the downloaded klines from binance as csv/pickle/feather
+
+    NOTE: feather have the highest compression and access time. csv option
+            was added to make accessing the file outside python is easier. pickle
+            option was added if feather is not usable on the host computer.
+    
+    """
+
+    # specify the column for the klines
+    columns = [
+        "open_time",
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+        "close_time",
+        "quote_asset_volume",
+        "number_of_trades",
+        "taker_buy_base_asset_volume",
+        "taker_buy_quote_asset_volume",
+        "ignore"
+    ]
+
+    # convert the date time str to <class 'datetime.datetime'>
+    start = parse(start).replace(tzinfo=pytz.UTC)
+    end = parse(end).replace(tzinfo=pytz.UTC)
+    # convert the datetime to timestamp (utc)
+    start = int(datetime.timestamp(start))
+    end = int(datetime.timestamp(end))
+
+    if (file_format == "csv"):
+
+        # write the column and klines as csv file
+        with open(pair + "_" + interval + "_(" + str(start) + "-" + str(end) + ").csv", "w", newline="") as f:
+            write = csv.writer(f)
+            write.writerow(columns)
+            write.writerows(rearranged_klines)
+
+    elif (file_format == "pickle"):
+
+        # as pickle
+        # NOTE: reinsert the column as the first element in the list
+        rearranged_klines.insert(0, columns)
+        with open(pair + "_" + interval + "_(" + str(start) + "-" + str(end) + ").pickle", "wb") as handle:
+            pickle.dump(rearranged_klines, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    elif (file_format == "feather"):
+
+        # convert nested list into a dataframe
+        df = pd.DataFrame(data=rearranged_klines, columns=columns)
+        # write the dataframe as feather file
+        df.to_feather(pair + "_" + interval + "_(" + str(start) + "-" + str(end) + ").feather", compression="zstd")
---
+++ source/01_exchange/cex_binance/new_version/BinanceHistoricalData/companion_code/time_splitter.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Mohd Hafizuddin Bin Kamilin"
+__date__ = "7 March 2022"
+
+# converting datetime in str into an object
+from dateutil.parser import parse
+# to get the current datetime
+from datetime import datetime
+# timezone manipulation
+import pytz
+
+def time_splitter(start: str, end: str, interval_value: int, duration_limit: int, concurrent_limit: int) -> tuple[list[list[str]], list[list[str]]]:
+
+    """
+    slice the time according to the duration_limit and group as chuck 
+
+    NOTE: we need to splice time duration specified by the user into month and
+        group it as a chunk of 10 months, to prevent exceeding 1200 request
+        weight allowed by binance; 10 months → 10 concurrent fetch call where
+        each retrieve 1 month worth of data → 450-950 request weight
+    
+    """
+
+    def list_segmenter(concurrent_limit: int, l: list) -> list[list]:
+
+        """
+        divide the splitted time (splitted_start and splitted_end) into a
+        nested list, where each of the nested list has int(concurrent_limit) length
+
+        """
+        
+        processed = []
+
+        # looping till length l
+        for i in range(0, len(l), concurrent_limit):
+            processed.append(l[i:i + concurrent_limit])
+
+        return processed
+
+    # to record the splitted time
+    splitted_start = []
+    splitted_end = []
+
+    timestamp_in_1m = 60
+
+    delay = False
+
+    # we specifically use UTC timezone to match with the binance API timezone
+    tz = pytz.timezone("UTC")
+    # convert the date time str to <class 'datetime.datetime'>
+    start_time = parse(start).replace(tzinfo=pytz.UTC)
+    time_duration = parse(end).replace(tzinfo=pytz.UTC)
+    # convert the datetime to timestamp (utc)
+    start_time = int(datetime.timestamp(start_time))
+    time_duration = int(datetime.timestamp(time_duration) - start_time)
+
+    # if the time_duration is more than duration_limit and more than the (interval_value * concurrent_limit)
+    if (time_duration > duration_limit) and (time_duration > interval_value * concurrent_limit):
+
+        # exceeded the maximum weight, thus adding delay before repeating
+        delay = True
+
+        # find out how many times we can divide the time_duration with the duration_limit and its remainder
+        quotient = int(time_duration / duration_limit)
+        remainder = time_duration % duration_limit
+
+        for i in range(quotient * concurrent_limit):
+
+            # append the newly calculated start time to the splitted_start
+            if i == 0:
+                splitted_start.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+            else:
+                # NOTE: smallest klines interval is 1 minute
+                splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
+
+            # append the newly calculated end time to the splitted_end
+            start_time += duration_limit / concurrent_limit
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+
+        # if there is a remainder from the division
+        if remainder != 0:
+
+            # append the newly calculated start time to the splitted_start
+            splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
+
+            # append the newly calculated end time to the splitted_end
+            start_time += remainder
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+
+        # create a nested list of `self.concurrent_limit` months
+        splitted_start = list_segmenter(concurrent_limit, splitted_start)
+        splitted_end = list_segmenter(concurrent_limit, splitted_end)
+
+    # else if the time_duration is more more than the (interval_value * concurrent_limit)
+    elif (time_duration > interval_value * concurrent_limit):
+
+        # find out how many times we can divide the time_duration with the concurrent_limit and its remainder
+        remainder = time_duration % concurrent_limit
+        quotient = int(time_duration / concurrent_limit)
+        number_of_loop = (concurrent_limit if (remainder == 0) else (concurrent_limit - 1))
+        
+        for i in range(number_of_loop):
+
+            # append the newly calculated start time to the splitted_start
+            if i == 0:
+                splitted_start.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+            else:
+                # NOTE: smallest klines interval is 1 minute
+                splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
+
+            # append the newly calculated end time to the splitted_end
+            start_time += quotient
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+
+        # if there is a remainder from the division
+        if remainder != 0:
+
+            # append the newly calculated start time to the splitted_start
+            splitted_start.append(str(datetime.fromtimestamp(start_time + timestamp_in_1m, tz))[:-6])
+
+            # append the newly calculated end time to the splitted_end
+            start_time += remainder
+            splitted_end.append(str(datetime.fromtimestamp(start_time, tz))[:-6])
+
+        # create a nested list
+        splitted_start = [splitted_start]
+        splitted_end = [splitted_end]
+
+    else:
+        
+        # no calculation needed
+        splitted_start = [[start]]
+        splitted_end = [[end]]
+
+    return splitted_start, splitted_end, delay
